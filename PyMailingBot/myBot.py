696e657d2484ed0e7e01f9206daa21095ab8e267
Harendra Singh <harendrasingh2205@gmail.com>
2019-03-02 15:32:51 +0530

    Don't remember what was changed. Was a pending change.

--- PyMailingBot/myBot.py
+++ PyMailingBot/myBot.py
@@ -2,14 +2,14 @@
 #     [
 #         {
 #             "title" : "Name of Event",
-#             "decsription" : "Some details about the event",
+#             "description" : "Some details about the event",
 #             "startTime" : "2019-02-27 09:43:29.826000", #In IST, format of Py datetime.datetime.now()
 #             "host" : "CDK Global"
 #         }
         
 #         {
 #             "title" : "Name of Event",
-#             "decsription" : "Some details about the event",
+#             "description" : "Some details about the event",
 #             "startTime" : "2019-02-27 09:43:29.826000", #In IST, format of Py datetime.datetime.now()
 #             "host" : "CDK Global"
 #         }
@@ -17,12 +17,22 @@
 
 import json
 import requests
+import datetime
 
-response=requests.get("https://localhost/events/api")
-events=json.loads(response.txt)
+response=requests.get("http://localhost:4000/api/pythonBot")
+#events=json.loads(response)
+events=response.json()
+#print(events[0]['title'])
+
+for event in events:
+	eventTitle=event['title']
+	eventStartTime=str(event['startTime'])
+	eventHost=event['host']
+	eventDescription=event['decsription']#need to correct the spelling
 
 
-mailSubject=eventTitle+" is happening tomorrow !"~
-mailBody="This is a reminder that "+eventTitle+" hosted by "+eventHost+" is going to start in the next 24 hours ! Do check out the event !\n"
-mailBody=mailBody+"\n\n"+eventDescription+"\n\n Starts at "+str(eventStartTime)+"."#The body of the mail
+	mailSubject=eventTitle+" is happening tomorrow !"
+	mailBody="This is a reminder that "+eventTitle+" hosted by "+eventHost+" is going to start in the next 24 hours ! Do check out the event !\n"
+	mailBody=mailBody+"\n\n"+eventDescription+"\n\n Starts at "+str(eventStartTime)+"."#The body of the mail
 
+	print(mailSubject,mailBody)
